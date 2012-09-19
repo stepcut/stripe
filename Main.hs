89f79970b4f0d50c7a20e62199a7d7d8c387752a
jeremy <jeremy@n-heptane.com>
2012-09-19 22:28:48 +0000

    fixes to createCharge

--- Main.hs
+++ Main.hs
@@ -1,14 +1,44 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Data.Text.Lazy as Text
+import qualified Data.Text.Lazy.Encoding as Text
 import Network.HTTP.Conduit
 import Stripe
 
 testKey :: ApiKey
 testKey = ApiKey "sk_TZjqQX1iWkWgMOuJivz4uElPsAkoI"
 
-main :: IO ()
-main =
+charges_1 :: IO ()
+charges_1 =
     do res <- withManager $ stripe testKey (charges Nothing Nothing Nothing)
+       print res
+
+dummyCard :: CardInfo
+dummyCard = CardInfo
+    { cardInfoNumber      = "4242424242424242"
+    , cardInfoExpMonth    = 01
+    , cardInfoExpYear     = 20
+    , cardInfoCvc         = Nothing
+    , cardInfoName        = Nothing
+    , cardInfoAddr1       = Nothing
+    , cardInfoAddr2       = Nothing
+    , cardInfoAddrZip     = Nothing
+    , cardInfoAddrState   = Nothing
+    , cardInfoAddrCountry = Nothing
+    }
+
+dummyCardToken :: CardToken
+dummyCardToken = CardToken "tok_jOq0M8vJprCUUU"
+
+create_charge_1 :: IO ()
+create_charge_1 =
+    do let sr@(StripeReq req) = createCharge 100 usd (CI dummyCard) (Just "a test charge")
+       print $ method req
+       print $ path req
+       print $ queryString req 
+       let (RequestBodyLBS lbs) = requestBody req
+       putStrLn $ Text.unpack $ Text.decodeUtf8 lbs
+       res <- withManager $ stripe testKey sr
        print res
 
--- Stripe.hs
+++ Stripe.hs
@@ -1,20 +1,36 @@
-{-# LANGUAGE DeriveDataTypeable, FlexibleContexts, GeneralizedNewtypeDeriving, OverloadedStrings, TemplateHaskell, TypeFamilies #-}
+{-# LANGUAGE DeriveDataTypeable, FlexibleContexts, GeneralizedNewtypeDeriving, OverloadedStrings, TemplateHaskell, TypeFamilies, RecordWildCards #-}
 module Stripe where
 
 import Control.Applicative ((<$>), (<*>))
 import Control.Monad        (mzero)
 import Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy as BL
 import Data.Aeson
 import Data.Conduit
 import Data.Data     (Data, Typeable)
 import Data.Maybe
+import Data.Monoid
 import Data.SafeCopy (SafeCopy, base, deriveSafeCopy)
 import Data.String   (fromString)
-import Data.Text     (Text)
+import Data.Text     as Text (Text, unpack)
 import qualified Data.Text.Encoding as Text
 import Control.Monad.Trans.Control
 import Network.HTTP.Conduit
 import qualified Network.HTTP.Types as W
+
+toStrict :: BL.ByteString -> ByteString
+toStrict = mconcat . BL.toChunks
+
+-- WARNING: only safe on ascii
+showBS :: (Show a) => a -> ByteString
+showBS = fromString . show
+
+mbParam :: ByteString -> Maybe a -> (a -> ByteString) -> Maybe (ByteString, ByteString)
+mbParam _ Nothing _ = Nothing
+mbParam name (Just v) show' = Just (name, show' v)
+
+usd :: Text
+usd = "usd"
 
 newtype ApiKey = ApiKey { unApiKey :: ByteString }
     deriving (Eq, Ord, Read, Show, Data, Typeable, SafeCopy)
@@ -198,15 +214,85 @@ charges mCount mOffset mCustomerId =
                          , mbParam "offset" mOffset (fromString . show)
                          , mbParam "customer" mCustomerId (Text.encodeUtf8 . unCustomerId)
                          ]
-      mbParam :: ByteString -> Maybe a -> (a -> ByteString) -> Maybe W.SimpleQueryItem
-      mbParam _ Nothing _ = Nothing
-      mbParam name (Just v) show' = Just (name, show' v)
 
+type Currency = Text
 
+data CardInfo = CardInfo
+    { cardInfoNumber      :: Text
+    , cardInfoExpMonth    :: Int
+    , cardInfoExpYear     :: Int
+    , cardInfoCvc         :: Maybe Int
+    , cardInfoName        :: Maybe Text
+    , cardInfoAddr1       :: Maybe Text
+    , cardInfoAddr2       :: Maybe Text
+    , cardInfoAddrZip     :: Maybe Text
+    , cardInfoAddrState   :: Maybe Text
+    , cardInfoAddrCountry :: Maybe Text
+    }
+    deriving (Eq, Ord, Read, Show, Data, Typeable)
+$(deriveSafeCopy 0 'base ''CardInfo)
+
+-- FIXME: for Nothing fields, should we send them with the value null or just not send them at all?
+instance ToJSON CardInfo where
+    toJSON CardInfo{..} =
+        object [ "number"          .= cardInfoNumber
+               , "exp_month"       .= cardInfoExpMonth
+               , "exp_year"        .= cardInfoExpYear
+               , "cvc"             .= cardInfoCvc
+{-
+               , "name"            .= cardInfoName
+               , "address_line1"   .= cardInfoAddr1
+               , "address_line2"   .= cardInfoAddr2
+               , "address_zip"     .= cardInfoAddrZip
+               , "address_state"   .= cardInfoAddrState
+               , "address_country" .= cardInfoAddrCountry 
+-}
+               ]
+
+newtype CardToken = CardToken { unCardToken :: Text }
+    deriving (Eq, Ord, Read, Show, Data, Typeable, SafeCopy)
+
+data ChargeTo
+    = CI CardInfo
+    | CT CardToken
+    | CS CustomerId
+    deriving (Eq, Ord, Read, Show, Data, Typeable)
+$(deriveSafeCopy 0 'base ''ChargeTo)
+
+createCharge :: (Monad m)
+             => Cents  -- ^ amount
+             -> Currency 
+             -> ChargeTo
+             -> Maybe Text -- ^ description
+             -> StripeReq m Charge
+createCharge amount currency chargeTo description =
+    let (Just req) = parseUrl "https://api.stripe.com/v1/charges"
+    in StripeReq $ urlEncodedBody params req
+    where
+      params = -- catMaybes 
+               [ ("amount", showBS amount)
+               , ("currency", Text.encodeUtf8 currency)
+               ] ++ 
+               (case chargeTo of
+                   (CS (CustomerId ci)) ->
+                       [ ("customer", Text.encodeUtf8 ci)]
+                   (CT (CardToken ct)) ->
+                       [ ("card", Text.encodeUtf8 ct)]
+                   (CI (CardInfo{..})) ->
+                       [ ("card[number]", Text.encodeUtf8 cardInfoNumber)
+                       , ("card[exp_month]", showBS cardInfoExpMonth)
+                       , ("card[exp_year]", showBS cardInfoExpYear)
+                       ]
+               )
+--               [ mbParam "description" description Text.encodeUtf8
+--               ]
+               
 stripe :: ( MonadResource m
           , MonadBaseControl IO m
           , FromJSON a
-          ) => ApiKey -> StripeReq m a -> Manager -> m (Maybe a)
+          ) => ApiKey -> StripeReq m a -> Manager -> m (Either String (Maybe a))
 stripe (ApiKey k) (StripeReq req) manager =
-    do res <- httpLbs (applyBasicAuth k "" req) manager
-       return $ decode' (responseBody res)
+    do res <- httpLbs (applyBasicAuth k "" (req { checkStatus = \_ _ -> Nothing})) manager
+       if W.statusCode (responseStatus res) == 200
+          then return $ Right $ decode' (responseBody res)
+          else return $ Left $ Text.unpack $ Text.decodeUtf8 $ toStrict $ responseBody  res
