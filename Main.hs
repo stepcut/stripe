98147b449d9a8b65f92a4f70ce0eedc36735fb5e
jeremy <jeremy@n-heptane.com>
2012-09-21 16:35:25 +0000

    generalize StripeReq so that it is less backend dependent

--- Main.hs
+++ Main.hs
@@ -32,22 +32,43 @@ dummyCard = CardInfo
     , cardInfoAddrCountry = Nothing
     }
 
+invalidCard :: CardInfo
+invalidCard = CardInfo
+    { cardInfoNumber      = "4242424242424241"
+    , cardInfoExpMonth    = 01
+    , cardInfoExpYear     = 20
+    , cardInfoCvc         = Nothing
+    , cardInfoName        = Nothing
+    , cardInfoAddr1       = Nothing
+    , cardInfoAddr2       = Nothing
+    , cardInfoAddrZip     = Nothing
+    , cardInfoAddrState   = Nothing
+    , cardInfoAddrCountry = Nothing
+    }
+
+
 dummyCardToken :: CardToken
 dummyCardToken = CardToken "tok_jOq0M8vJprCUUU"
 
 create_charge_1 :: IO ()
 create_charge_1 =
-    do let sr@(StripeReq req) = createCharge 100 usd (CI dummyCard) (Just "a test charge")
-       print $ method req
-       print $ path req
-       print $ queryString req 
-       let (RequestBodyLBS lbs) = requestBody req
-       putStrLn $ Text.unpack $ Text.decodeUtf8 lbs
+    do let sr = createCharge 100 usd (CI dummyCard) (Just "a test charge")
+       res <- withManager $ stripe testKey sr
+       print res
+
+create_charge_2 :: IO ()
+create_charge_2 =
+    do let sr = createCharge 100 usd (CI invalidCard) (Just "a test charge")
        res <- withManager $ stripe testKey sr
        print res
 
 retrieve_charge_1 =
     do res <- withManager $ stripe testKey (retrieveCharge (ChargeId "ch_0P5N4NiWnsDdhn"))
+       print res
+
+
+retrieve_charge_2 =
+    do res <- withManager $ stripe testKey (retrieveCharge (ChargeId "ch_5N4NiWnsDdhn"))
        print res
 
 
@@ -133,3 +154,4 @@ cardValue = case parseOnly json cardString of
     "object": "card",
     "type": "Visa"
   }|])
+
--- Stripe.hs
+++ Stripe.hs
@@ -9,6 +9,7 @@ import qualified Data.ByteString.Lazy as BL
 import Data.Aeson
 import Data.Conduit
 import Data.Data     (Data, Typeable)
+import qualified Data.HashMap.Strict as HashMap
 import Data.Maybe
 import Data.Monoid
 import Data.SafeCopy (SafeCopy, base, deriveSafeCopy)
@@ -30,6 +31,21 @@ mbParam :: ByteString -> Maybe a -> (a -> ByteString) -> Maybe (ByteString, Byte
 mbParam _ Nothing _ = Nothing
 mbParam name (Just v) show' = Just (name, show' v)
 
+
+{-
+mbItem :: ByteString -> (a -> ByteString) -> Maybe a -> Maybe (ByteString, ByteString)
+mbItem k f Nothing = Nothing
+mbItem k f (Just v) = Just (k, f v)
+-}
+
+data StripeReq ret = StripeReq
+    { srUrl         :: String
+    , srQueryString :: [(ByteString, ByteString)]
+    , srPostData    :: Maybe [(ByteString, ByteString)]
+    }
+    deriving (Eq, Ord, Read, Show, Data, Typeable)
+$(deriveSafeCopy 0 'base ''StripeReq)
+
 usd :: Text
 usd = "usd"
 
@@ -37,7 +53,7 @@ newtype ApiKey = ApiKey { unApiKey :: ByteString }
     deriving (Eq, Ord, Read, Show, Data, Typeable, SafeCopy)
 
 data CardError
-    = IncorrectCardNumber
+    = IncorrectNumber
     | InvalidExpiryMonth
     | InvalidExpiryYear
     | InvalidCvc
@@ -49,20 +65,53 @@ data CardError
       deriving (Eq, Ord, Read, Show, Data, Typeable)
 $(deriveSafeCopy 0 'base ''CardError)
 
+instance FromJSON CardError where
+    parseJSON (String str)
+        | str == "incorrect_number"     = return IncorrectNumber
+        | str == "invalid_expiry_month" = return InvalidExpiryMonth
+        | str == "invalid_expiry_year"  = return InvalidExpiryYear
+        | str == "invalid_cvc"          = return InvalidCvc
+        | str == "expired_card"         = return ExpiredCard
+        | str == "incorrect_cvc"        = return IncorrectCvc
+        | str == "card_declined"        = return CardDeclined
+        | str == "missing"              = return Missing
+        | str == "processing_error"     = return ProcessingError
+        | otherwise                     = mzero
+    parseJSON _ = mzero
+
 data ErrorType
     = InvalidRequestError
     | ApiError
-    | CardError CardError
+    | CardError
       deriving (Eq, Ord, Read, Show, Data, Typeable)
 $(deriveSafeCopy 0 'base ''ErrorType)
+
+instance FromJSON ErrorType where
+    parseJSON (String str)
+        | str == "card_error" = return CardError
+        | str == "invalid_request_error" = return InvalidRequestError
+        | str == "api_error"  = return ApiError
+    parseJSON _ = mzero
 
 data StripeError = StripeError
     { errorType :: ErrorType
     , message   :: Text
+    , code      :: Maybe CardError
     , param     :: Maybe Text
     }
     deriving (Eq, Ord, Read, Show, Data, Typeable)
 $(deriveSafeCopy 0 'base ''StripeError)
+
+instance FromJSON StripeError where
+    parseJSON (Object obj') =
+        case HashMap.lookup "error" obj' of
+          (Just (Object obj)) ->
+              StripeError <$> obj .: "type"
+                          <*> obj .: "message"
+                          <*> obj .:? "code"
+                          <*> obj .:? "param"
+          _ -> mzero
+    parseJSON _ = mzero
 
 newtype CustomerId = CustomerId { unCustomerId :: Text }
     deriving (Eq, Ord, Read, Show, Data, Typeable, SafeCopy, FromJSON)
@@ -204,18 +253,21 @@ instance FromJSON Charges where
                 <*> obj .: "data"
     parseJSON _ = mzero
 
-type Count = Integer
+type Count  = Integer
 type Offset = Integer
 
-newtype StripeReq m ret = StripeReq { unSR :: Request m }
-
-charges :: Maybe Count -> Maybe Offset -> Maybe CustomerId -> StripeReq m Charges
+charges :: Maybe Count
+        -> Maybe Offset
+        -> Maybe CustomerId
+        -> StripeReq Charges
 charges mCount mOffset mCustomerId =
-    let (Just req) = parseUrl "https://api.stripe.com/v1/charges"
-    in StripeReq $ req { queryString = W.renderSimpleQuery False params  }
+    StripeReq { srUrl         = "https://api.stripe.com/v1/charges"
+              , srQueryString = params
+              , srPostData    = Nothing
+              }
     where
-      params = catMaybes [ mbParam "count" mCount (fromString . show)
-                         , mbParam "offset" mOffset (fromString . show)
+      params = catMaybes [ mbParam "count"    mCount      showBS
+                         , mbParam "offset"   mOffset     showBS
                          , mbParam "customer" mCustomerId (Text.encodeUtf8 . unCustomerId)
                          ]
 
@@ -246,17 +298,18 @@ data ChargeTo
     deriving (Eq, Ord, Read, Show, Data, Typeable)
 $(deriveSafeCopy 0 'base ''ChargeTo)
 
-createCharge :: (Monad m)
-             => Cents  -- ^ amount
+createCharge :: Cents  -- ^ amount
              -> Currency 
              -> ChargeTo
              -> Maybe Text -- ^ description
-             -> StripeReq m Charge
+             -> StripeReq Charge
 createCharge amount currency chargeTo description =
-    let (Just req) = parseUrl "https://api.stripe.com/v1/charges"
-    in StripeReq $ urlEncodedBody params req
+    StripeReq { srUrl         = "https://api.stripe.com/v1/charges"
+              , srQueryString = []
+              , srPostData    = Just params
+              }
     where
-      params = -- catMaybes 
+      params =
                [ ("amount", showBS amount)
                , ("currency", Text.encodeUtf8 currency)
                ] ++ 
@@ -265,43 +318,132 @@ createCharge amount currency chargeTo description =
                        [ ("customer", Text.encodeUtf8 ci)]
                    (CT (CardToken ct)) ->
                        [ ("card", Text.encodeUtf8 ct)]
-                   (CI (CardInfo{..})) ->
-                       catMaybes [ Just  ("card[number]", Text.encodeUtf8 cardInfoNumber)
-                                 , Just  ("card[exp_month]", showBS cardInfoExpMonth)
-                                 , Just  ("card[exp_year]", showBS cardInfoExpYear)
-                                 , mbItem "card[cvc]" showBS cardInfoCvc
-                                 , mbItem "card[name]" Text.encodeUtf8 cardInfoName
-                                 , mbItem "card[address_line1]" Text.encodeUtf8 cardInfoAddr1
-                                 , mbItem "card[address_line2]" Text.encodeUtf8 cardInfoAddr2
-                                 , mbItem "card[address_zip]" Text.encodeUtf8 cardInfoAddrZip
-                                 , mbItem "card[address_state]" Text.encodeUtf8 cardInfoAddrState
-                                 , mbItem "card[address_country]" Text.encodeUtf8 cardInfoAddrCountry
-                                 ]
+                   (CI ci) -> cardInfoPairs ci
                )
 
-mbItem k f Nothing = Nothing
-mbItem k f (Just v) = Just (k, f v)
+cardInfoPairs :: CardInfo -> [(ByteString, ByteString)]
+cardInfoPairs (CardInfo{..}) =
+    catMaybes [ Just  ("card[number]", Text.encodeUtf8 cardInfoNumber)
+              , Just  ("card[exp_month]", showBS cardInfoExpMonth)
+              , Just  ("card[exp_year]", showBS cardInfoExpYear)
+              , mbParam "card[cvc]"             cardInfoCvc         showBS 
+              , mbParam "card[name]"            cardInfoName        Text.encodeUtf8 
+              , mbParam "card[address_line1]"   cardInfoAddr1       Text.encodeUtf8 
+              , mbParam "card[address_line2]"   cardInfoAddr2       Text.encodeUtf8 
+              , mbParam "card[address_zip]"     cardInfoAddrZip     Text.encodeUtf8 
+              , mbParam "card[address_state]"   cardInfoAddrState   Text.encodeUtf8 
+              , mbParam "card[address_country]" cardInfoAddrCountry Text.encodeUtf8 
+              ]
 
-retrieveCharge :: (Monad m) => ChargeId -> StripeReq m Charge
+
+retrieveCharge :: ChargeId
+               -> StripeReq Charge
 retrieveCharge cid =
-    let (Just req) = parseUrl $ "https://api.stripe.com/v1/charges/" ++ (Text.unpack $ unChargeId cid)
-    in StripeReq req
+    StripeReq { srUrl         = "https://api.stripe.com/v1/charges/" ++ (Text.unpack $ unChargeId cid)
+              , srQueryString = []
+              , srPostData    = Nothing
+              }
 
-refundCharge :: (Monad m) => ChargeId -> Maybe Cents -> StripeReq m Charge
+
+refundCharge :: ChargeId
+             -> Maybe Cents
+             -> StripeReq Charge
 refundCharge cid mCents =
-    let (Just req) = parseUrl $ "https://api.stripe.com/v1/charges/" ++ (Text.unpack $ unChargeId cid) ++ "/refund"
-    in case mCents of
-         Nothing      -> StripeReq $ req { method = "POST" }
-         (Just cents) -> StripeReq $ urlEncodedBody [("amount", showBS cents)] req
-    
-               
+    StripeReq { srUrl         = "https://api.stripe.com/v1/charges/" ++ (Text.unpack $ unChargeId cid) ++ "/refund"
+              , srQueryString = []
+              , srPostData    = Just $ case mCents of
+                                         Nothing      -> []
+                                         (Just cents) -> [("amount", showBS cents)]
+              }
+
+------------------------------------------------------------------------------
+-- Customer
+------------------------------------------------------------------------------
+
+data Customer = Customer
+    { customerId :: CustomerId
+    , customerLivemode :: Bool
+    , customerCreated  :: Timestamp
+    , customerAccountBalance :: Maybe Integer
+    , customerActiveCard :: Card
+    , customerDeliquent :: Maybe Bool
+    , customerDescription :: Maybe Text
+--    , customerDiscount :: Maybe Discount
+    , customerEmail :: Maybe Text
+--    , customerSubscription :: Maybe Subscription
+    }
+    deriving (Eq, Ord, Read, Show, Data, Typeable)
+$(deriveSafeCopy 0 'base ''Customer)
+
+instance FromJSON Customer where
+    parseJSON (Object obj) =
+        Customer <$> obj .: "id"
+                 <*> obj .: "livemode"
+                 <*> obj .: "created"
+                 <*> obj .: "account_balance"
+                 <*> obj .: "active_card"
+                 <*> obj .: "delinquent"
+                 <*> obj .: "description"
+--                 <*> obj .: "discount"
+                 <*> obj .: "email"
+--                 <*> obj .: "subscription"
+    parseJSON _ = mzero
+
+createCustomer :: Either CardInfo CardToken 
+               -> Maybe Coupon
+               -> Maybe Text -- ^ email
+               -> Maybe Text -- ^ description
+               -> Maybe Integer -- ^ acount balance
+               -> Maybe Plan
+               -> Maybe Timestamp -- ^ trial end
+               -> Maybe Integer -- ^ quantity
+               -> StripeReq Customer
+createCustomer = undefined
+
+------------------------------------------------------------------------------
+-- Discount
+------------------------------------------------------------------------------
+
+data Discount = Discount
+
+------------------------------------------------------------------------------
+-- Subscription
+------------------------------------------------------------------------------
+
+data Subscription = Subscription
+
+------------------------------------------------------------------------------
+-- Coupon
+------------------------------------------------------------------------------
+
+data Coupon = Coupon
+
+------------------------------------------------------------------------------
+-- Plan
+------------------------------------------------------------------------------
+
+data Plan = Plan
+
+
+------------------------------------------------------------------------------
+-- 
+------------------------------------------------------------------------------
+
 stripe :: ( MonadResource m
           , MonadBaseControl IO m
           , FromJSON a
-          ) => ApiKey -> StripeReq m a -> Manager -> m (Either String (Maybe a))
-stripe (ApiKey k) (StripeReq req) manager =
-    do res <- httpLbs (applyBasicAuth k "" (req { checkStatus = \_ _ -> Nothing})) manager
+          ) => ApiKey
+       -> StripeReq a
+       -> Manager
+       -> m (Either StripeError a)
+stripe (ApiKey k) (StripeReq{..}) manager =
+    do let req = maybe id urlEncodedBody srPostData $
+                  (fromJust $ parseUrl srUrl)
+                    { queryString = W.renderSimpleQuery False srQueryString 
+                    } 
+       res <- httpLbs (applyBasicAuth k "" (req { checkStatus = \_ _ -> Nothing})) manager
+       liftIO $ print $ responseStatus res
        liftIO $ putStrLn $ Text.unpack $ Text.decodeUtf8 $ toStrict $ responseBody  res
        if W.statusCode (responseStatus res) == 200
-          then return $ Right $ decode' (responseBody res)
-          else return $ Left $ Text.unpack $ Text.decodeUtf8 $ toStrict $ responseBody  res
+          then return $ Right $ fromJust $ decode' (responseBody res)
+          else return $ Left  $ fromJust $ decode' (responseBody res)
